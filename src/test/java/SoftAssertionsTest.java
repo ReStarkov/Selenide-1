704701cbd75d70b877d8674357595ed73e84876e
roman <re_starkov@betboom.org>
2022-12-25 21:29:37 +0400

    added test for search softAssertions

--- src/test/java/SoftAssertionsTest.java
+++ src/test/java/SoftAssertionsTest.java
@@ -21,13 +21,5 @@ public class SoftAssertionsTest {
 
         // внутри есть пример кода для JUnit5
         $("div#wiki-body").shouldHave(text("3. Using JUnit5 extend test class:"));
-
-
-
-
-
-
-
     }
-
 }
