8c3b9622ff576c15733f541285a5d6aba69a7419
roman <re_starkov@betboom.org>
2023-02-04 18:45:16 +0400

    added tests with steps and config for selenoid

--- src/test/java/demoqa/tests/BaseTest.java
+++ src/test/java/demoqa/tests/BaseTest.java
@@ -1,9 +1,16 @@
 package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import demoqa.helpers.Attach;
 import demoqa.pages.RegistrationPage;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class BaseTest {
 
@@ -11,8 +18,32 @@ public class BaseTest {
 
     RegistrationPage registrationPage = new RegistrationPage();
     @BeforeAll
-    public static void setUp(){
+    static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize="1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
--- src/test/java/demoqa/tests/PracticeFormTest.java
+++ src/test/java/demoqa/tests/PracticeFormTest.java
@@ -1,40 +1,96 @@
 package demoqa.tests;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class PracticeFormTest extends BaseTest {
 
     @Test
+    @Tag("form")
     void practiceFormTest(){
 
-        //заполнение формы
-        registrationPage.openPage()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setMail(testData.mail)
-                .setGender(testData.gender)
-                .setPhone(testData.phone)
-                .setBirthDate(testData.day, testData.month, testData.year)
-                .setSubject(testData.subject)
-                .setHobbies(testData.hobbies)
-                .uploadImage(testData.pathToImage)
-                .setAddress(testData.currentAddress)
-                .setState(testData.state)
-                .setCity(testData.city)
-                .submitForm();
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        //проверка результатов
-        registrationPage.verifyModalAppears()
-                .verifyResult("Student Name",testData.firstName + " " + testData.lastName)
-                .verifyResult("Student Email",testData.mail)
-                .verifyResult("Gender", testData.gender)
-                .verifyResult("Mobile", testData.phone)
-                .verifyResult("Date of Birth", testData.dateBirth)
-                .verifyResult("Subjects", testData.subject)
-                .verifyResult("Hobbies", testData.hobbies)
-                .verifyResult("Picture", testData.image)
-                .verifyResult("Address", testData.currentAddress)
-                .verifyResult("State and City", testData.state + " " + testData.city);
+        step("Открытие страницы с формой", () -> {
+            registrationPage.openPage();
+        });
+        step("Ввод имени", () ->{
+            registrationPage.setFirstName(testData.firstName);
+        });
+        step("Ввод фамилии", () ->{
+            registrationPage.setLastName(testData.lastName);
+        });
+        step("Ввод email", () ->{
+            registrationPage.setMail(testData.mail);
+        });
+        step("Выбор пола", () ->{
+            registrationPage.setGender(testData.gender);
+        });
+        step("Ввод телефонного номера", () ->{
+            registrationPage.setPhone(testData.phone);
+        });
+        step("Выбор даты рождения", () ->{
+            registrationPage.setBirthDate(testData.day, testData.month, testData.year);
+        });
+        step("Выбор предметов", () ->{
+            registrationPage.setSubject(testData.subject);
+        });
+        step("Выбор хобби", () ->{
+            registrationPage.setHobbies(testData.hobbies);
+        });
+        step("Загрузка изображения", () ->{
+            registrationPage.uploadImage(testData.pathToImage);
+        });
+        step("Ввод адресса", () ->{
+            registrationPage.setAddress(testData.currentAddress);
+        });
+        step("Выбор штата", () ->{
+            registrationPage.setState(testData.state);
+        });
+        step("Выбор города", () ->{
+            registrationPage.setCity(testData.city);
+        });
+        step("Нажатие на кнопку submit", () ->{
+            registrationPage.submitForm();
+        });
+
+
+        step("Проверка результата: Открытие модального окна с результатами", () ->{
+            registrationPage.verifyModalAppears();
+        });
+        step("Проверка результата: Имя и фамилия соответствует введенному значению", () ->{
+            registrationPage.verifyResult("Student Name",testData.firstName + " " + testData.lastName);
+        });
+        step("Проверка результата: Email соответствует введенному значению", () ->{
+            registrationPage.verifyResult("Student Email",testData.mail);
+        });
+        step("Проверка результата: Пол соответствует выбранному значению", () ->{
+            registrationPage.verifyResult("Gender", testData.gender);
+        });
+        step("Проверка результата: Телефон соответствует введенному значению", () ->{
+            registrationPage.verifyResult("Mobile", testData.phone);
+        });
+        step("Проверка результата: Дата рождения соответствует выбранному значению", () ->{
+            registrationPage.verifyResult("Date of Birth", testData.dateBirth);
+        });
+        step("Проверка результата: Предмет соответствует выбранному значению", () ->{
+            registrationPage.verifyResult("Subjects", testData.subject);
+        });
+        step("Проверка результата: Хобби соответствует выбранному значению", () ->{
+            registrationPage.verifyResult("Hobbies", testData.hobbies);
+        });
+        step("Проверка результата: Название изображения соответствует выбранному значению", () ->{
+            registrationPage.verifyResult("Picture", testData.image);
+        });
+        step("Проверка результата: Адресс соответствует введенному значению", () ->{
+            registrationPage.verifyResult("Address", testData.currentAddress);
+        });
+        step("Проверка результата: Город и штат соответствует выбранному значению", () ->{
+            registrationPage.verifyResult("State and City", testData.state + " " + testData.city);
+        });
     }
 }
